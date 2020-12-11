b1d41ed054322147fc6ada73b692ce5807644299
Rietts Andreas Ruff <Riettsruff@gmail.com>
2020-12-11 16:21:24 +0700

    Add validation service and update api service

--- src/main/resources/static/js/services/api.js
+++ src/main/resources/static/js/services/api.js
@@ -1,6 +1,6 @@
 const API_URI = "http://206.189.94.183:8085";
 
-async function AXIOS_CUSTOM(method, baseURL, data = null) {
+async function axiosCustom(method, baseURL, data = null) {
   const request = await axios({
     method,
     baseURL,
@@ -15,34 +15,34 @@ async function AXIOS_CUSTOM(method, baseURL, data = null) {
 // Cuti Endpoint
 
 function saveCuti(data) {
-	return AXIOS_CUSTOM("POST", `${API_URI}/cuti/save`, data);
+	return axiosCustom("POST", `${API_URI}/cuti/save`, data);
 }
 
 
 // Departemen Endpoint
 
 function saveDepartemen(data) {
-	return AXIOS_CUSTOM("POST", `${API_URI}/departemen`, data);
+	return axiosCustom("POST", `${API_URI}/departemen`, data);
 }
 
 
 // Jenis Cuti Endpoint
 
 function saveJenisCuti(data) {
-	return AXIOS_CUSTOM("POST", `${API_URI}/jeniscuti`, data);
+	return axiosCustom("POST", `${API_URI}/jeniscuti`, data);
 }
 
 
 // Karyawan Endpoint
 
 function saveKaryawan(data) {
-	return AXIOS_CUSTOM("POST", `${API_URI}/karyawan`, data);
+	return axiosCustom("POST", `${API_URI}/karyawan`, data);
 }
 
 function updateKaryawan(data) {
-	return AXIOS_CUSTOM("PUT", `${API_URI}/karyawan/${data.id}`, data);
+	return axiosCustom("PUT", `${API_URI}/karyawan/${data.id}`, data);
 }
 
 function deleteKaryawan(id) {
-	return AXIOS_CUSTOM("DELETE", `${API_URI}/karyawan/${id}`);
+	return axiosCustom("DELETE", `${API_URI}/karyawan/${id}`);
 }
---
+++ src/main/resources/static/js/services/validation.js
@@ -0,0 +1,34 @@
+const NUMERIC_REGEX = /^[0-9]*$/;
+
+const URL_REGEX = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function inputValidation(htmlInput) {
+  return new Promise((resolve, reject) => {
+    if(htmlInput.data("required") && !htmlInput.val()) {
+      return reject(`${htmlInput.data("label")} wajib diisi`);
+    }
+
+    if(htmlInput.data("format") && htmlInput.data("format") !== "PLAIN" && htmlInput.val()) {
+      switch(htmlInput.data("format")) {
+        case "NUMERIC":
+          if(!NUMERIC_REGEX.test(htmlInput.val())) {
+            return reject(`${htmlInput.data("label")} wajib berupa angka`);
+          } 
+        break;
+        case "URL":
+          if(!URL_REGEX.test(htmlInput.val())) {
+            return reject(`Format ${htmlInput.data("label")} tidak valid`);
+          }
+        break;
+        case "EMAIL":
+          if(!EMAIL_REGEX.test(htmlInput.val())) {
+            return reject(`Format ${htmlInput.data("label")} tidak valid`);
+          }
+      }
+    }
+
+    return resolve("Valid");
+  });
+}
